125183589e1c96ab792c7d8e395f17522b8ce0b1
Farah Farah <afarah1@css.edu>
2017-11-11 20:26:03 -0600

    Partialy done asynchoronize. Still not asynchronize

---
+++ CurrencyTrader.AdoNet/AsyncTradeStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyTrader.Contracts;
+
+namespace CurrencyTrader.AdoNet
+{
+    public class AsyncTradeStorage : ITradeStorage
+    {
+
+        private readonly ILogger logger;
+        private ITradeStorage SyngTradeStorage;
+
+        public AsyncTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            SyngTradeStorage = new AdoNetTradeStorage(logger);
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            logger.LogInfo("Starting synch trade storage");
+            SyngTradeStorage.Persist(trades);
+        }
+    }
+}
--- CurrencyTrader.Gui/Form1.cs
+++ CurrencyTrader.Gui/Form1.cs
@@ -38,7 +38,7 @@ namespace CurrencyTrader.Gui
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
 
-            var tradeStorage = new AdoNetTradeStorage(logger);
+            ITradeStorage tradeStorage = new AsyncTradeStorage(logger);
             tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
         }
 
